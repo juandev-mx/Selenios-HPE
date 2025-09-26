bbf47f1fcb4f5eb14a68ec04ce32ea8baaf7a41e
Abraham <avranlg2@gmail.com>
2025-09-26 12:10:54 -0600

    Actualizado

--- app.py
+++ app.py
@@ -79,8 +79,7 @@ def create_client_company():
 
     return jsonify({"created": created, "errors": errors}), 201
 
-#este conentario no es de chat joaquin
-#Nada
+
 @app.route('/client_company/<int:id>', methods=['PUT'])
 def update_client_company(id):
     company = ClientCompany.query.get_or_404(id)
